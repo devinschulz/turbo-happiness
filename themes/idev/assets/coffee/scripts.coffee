96d5185c0e5e7f61993fa024accbc4e9feb3a090
Devin Schulz <devin@devinschulz.com>
2014-10-06 09:21:48 -0400

    Add navigation

--- themes/idev/assets/coffee/scripts.coffee
+++ themes/idev/assets/coffee/scripts.coffee
@@ -1,2 +1,53 @@
+'use strict'
 
+datajs = (selector) ->
+  document.querySelectorAll "[data-js=" + selector + "]"
 
+Element::addClass = (className) ->
+  unless @hasClass className
+    @className += ' ' + className
+
+Element::hasClass = (className) ->
+  new RegExp(' ' + className + ' ').test ' ' + @className + ' '
+
+Element::removeClass = (className) ->
+  newClass = " " + @className.replace(/[\t\r\n]/g, " ") + " "
+  if @hasClass(className)
+    while newClass.indexOf(" " + className + " ") >= 0
+      newClass = newClass.replace(" " + className + " ", " ")
+    @className = newClass.replace(/^\s+|\s+$/g, " ")
+
+# Check if mobile
+isMobile = ->
+  return !!('ontouchstart' in window) or !!('msmaxtouchpoints' in navigator)
+
+navigation = document.querySelector '[data-js=nav-toggle]'
+navList = document.querySelector '[data-js=nav]'
+clicked = false
+
+navToggle = (event) ->
+  self = @
+  unless clicked
+    self.addClass 'is--open'
+    navList.addClass 'is--open'
+  else
+    self.removeClass 'is--open'
+    navList.removeClass 'is--open'
+  clicked = not clicked
+  event.preventDefault()
+
+if isMobile()
+  navigation.addEventListener('touchstart', navToggle, false)
+else
+  navigation.addEventListener('click', navToggle)
+
+###
+  Prevent Spam by Generating the Email
+###
+SetEmail = ->
+  element = document.querySelector('.header__nav__list__item--contact')
+  name = "devin"
+  email = "mailto:" + name + "@" + name + "schulz" + ".com"
+  link = element.firstElementChild
+  link.setAttribute("href", email)
+SetEmail()
--- themes/idev/static/js/scripts.js
+++ themes/idev/static/js/scripts.js
@@ -1,1 +1,74 @@
+'use strict';
+var SetEmail, clicked, datajs, isMobile, navList, navToggle, navigation,
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+datajs = function(selector) {
+  return document.querySelectorAll("[data-js=" + selector + "]");
+};
+
+Element.prototype.addClass = function(className) {
+  if (!this.hasClass(className)) {
+    return this.className += ' ' + className;
+  }
+};
+
+Element.prototype.hasClass = function(className) {
+  return new RegExp(' ' + className + ' ').test(' ' + this.className + ' ');
+};
+
+Element.prototype.removeClass = function(className) {
+  var newClass;
+  newClass = " " + this.className.replace(/[\t\r\n]/g, " ") + " ";
+  if (this.hasClass(className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    return this.className = newClass.replace(/^\s+|\s+$/g, " ");
+  }
+};
+
+isMobile = function() {
+  return !!(__indexOf.call(window, 'ontouchstart') >= 0) || !!(__indexOf.call(navigator, 'msmaxtouchpoints') >= 0);
+};
+
+navigation = document.querySelector('[data-js=nav-toggle]');
+
+navList = document.querySelector('[data-js=nav]');
+
+clicked = false;
+
+navToggle = function(event) {
+  var self;
+  self = this;
+  if (!clicked) {
+    self.addClass('is--open');
+    navList.addClass('is--open');
+  } else {
+    self.removeClass('is--open');
+    navList.removeClass('is--open');
+  }
+  clicked = !clicked;
+  return event.preventDefault();
+};
+
+if (isMobile()) {
+  navigation.addEventListener('touchstart', navToggle, false);
+} else {
+  navigation.addEventListener('click', navToggle);
+}
+
+
+/*
+  Prevent Spam by Generating the Email
+ */
+
+SetEmail = function() {
+  var element, email, link, name;
+  element = document.querySelector('.header__nav__list__item--contact');
+  name = "devin";
+  email = "mailto:" + name + "@" + name + "schulz" + ".com";
+  link = element.firstElementChild;
+  return link.setAttribute("href", email);
+};
+
+SetEmail();
